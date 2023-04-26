0b01f6fc018d66e13159a89caba8c056d647d3ef
ImCoderz <naamaabderrahmane2002@gmail.com>
2023-04-26 20:04:52 +0100

    portfolio

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -35,10 +35,10 @@ const Homepage = () => {
     }
 
     return (
-        <div className='sm:px-28 px-0 md:px-12 md:mt-20 mt-1 grid md:grid-cols-2 grid-col-1 md:gap-6 gap-20 place-items-center'>
+        <div className='px-0  md:mt-20 mt-1 flex md:flex-row flex-col-reverse md:justify-between justify-center lg:px-32 md:px-12  items-center  md:gap-6 gap-10 place-items-center'>
             <div className='relative items-center flex flex-col gap-3 animate-slideleft'>
                 <div>
-                    <h1 id='Home' className='gradient text-center lg:w-[250px] w-[220px] lg:text-xl text-lg font-bold'>Hi! I’m <br/> Abderrahmane Naama <br /> <span className='text-sm'>a software engineer student</span>.</h1>
+                    <h1 id='Home' className='gradient text-center lg:w-[250px] w-[250px]  lg:text-xl text-lg font-bold'>Hi! I’m <br/> Abderrahmane Naama <br /> <span className='text-sm'>a software engineer student</span>.</h1>
                 </div>
                 <div className='flex items-center flex-col gap-6 bg-purple3color w-fit rounded-[30px] p-4'>
                     <Row picture={inpt} text={'an INPT student'}/>
@@ -48,28 +48,28 @@ const Homepage = () => {
                 <div className='w-[230px] h-[230px] absolute top-32 right-16 blurpurple '></div>
             </div>
             
-            <div className=' grid relative place-items-center ml-12 animate-slideright transition-shadow duration-100'>
-                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-right-4 lg:-top-10 -right-4 -top-8  spin`}>
-                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full' src={List[0]} alt="" />
+            <div className=' grid relative place-items-center md:ml-12 animate-slideright transition-shadow duration-100'>
+                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-right-4 md:flex hidden lg:-top-10 -right-4 -top-8  spin`}>
+                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full ' src={List[0]} alt="" />
                 </div>
-                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:right-32 lg:-top-20 right-32 -top-16  spin`}>
-                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full' src={List[1]} alt="" />
+                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:right-32 md:flex hidden lg:-top-20 right-32 -top-16  spin`}>
+                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full ' src={List[1]} alt="" />
                 </div>
-                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-left-12 lg:-top-4 -left-8 -top-0  spin`}>
-                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full' src={List[2]} alt="" />
+                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-left-12 md:flex hidden lg:-top-4 -left-8 -top-0  spin`}>
+                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full ' src={List[2]} alt="" />
                 </div>
                 <div className={` lg:w-[300px] lg:h-[300px] w-[270px] h-[270px] rounded-full bg-purple2color  z-10 animate-slowfade`}>
                     <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full' src={List[6]} alt="" />
                 </div>
                 <div className='w-[370px] h-[370px] rounded-full absolute top-8 right-[-18px] blurpurple'></div>
-                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-left-24 lg:top-32 -left-16 top-32  spin`}>
-                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full' src={List[3]} alt="" />
+                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-left-24 md:flex hidden lg:top-32 -left-16 top-32  spin`}>
+                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full ' src={List[3]} alt="" />
                 </div>
-                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-left-8 lg:-bottom-8 -left-4 -bottom-4  spin`}>
-                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full' src={List[4]} alt="" />
+                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:-left-8  md:flex hidden lg:-bottom-8 -left-4 -bottom-4  spin`}>
+                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full ' src={List[4]} alt="" />
                 </div>
-                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:right-20 lg:-bottom-20 right-24 -bottom-16  spin`}>
-                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full' src={List[5]} alt="" />
+                <div className={`lg:w-[70px] w-[55px] lg:h-[70px] h-[55px] rounded-full bg-purple2color absolute lg:right-20 md:flex hidden lg:-bottom-20 right-24 -bottom-16  spin`}>
+                    <img className='object-cover pictureblur flex justify-center items-center rounded-full w-full h-full ' src={List[5]} alt="" />
                 </div>
             </div>
         </div>
